2dd8c30e12149d5593efbc401022ed1d3f5c27ab
Danylo <lliivadin@gmail.com>
2025-11-29 14:13:24 +0200

    fix: add check number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,15 @@ let totalPopulation = 0;
 let averagePopulation = 0;
 const populationList = document.querySelectorAll('.population');
 
-populationList.forEach((el) => {
-  const population = Number(el.textContent.replace(/,/g, ''));
-
-  totalPopulation += population;
+populationList.forEach(el => {
+    const population = Number(el.textContent.replace(/,/g, ''));
+    
+    if (!isNaN(population)) {
+        totalPopulation += population;
+    }
 });
-averagePopulation = totalPopulation / populationList.length;
 
-document.querySelector('.total-population').textContent =
-  totalPopulation.toLocaleString('en-US');
+averagePopulation = Math.round(totalPopulation / populationList.length);
 
-document.querySelector('.average-population').textContent =
-  averagePopulation.toLocaleString('en-US');
+document.querySelector('.total-population').textContent = totalPopulation.toLocaleString('en-US');
+document.querySelector('.average-population').textContent = averagePopulation.toLocaleString('en-US');
